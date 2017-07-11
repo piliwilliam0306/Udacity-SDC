fbff80370367470f2226cc58c1df14b26e0c4a37
piliwilliam0306 <piliwilliam0306@gmail.com>
2017-07-11 22:27:47 +0800

    add images

--- CarND-BehavioralCloning-P3/drive.py
+++ CarND-BehavioralCloning-P3/drive.py
@@ -71,13 +71,11 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        #image_array = image_array
-        image_array = ResizeImage(image_array)
+        resized_image = ResizeImage(image_array)
 
-
-        image_array = RGB_to_S(image_array)
-        image_array = image_array.reshape(1, 14, 64, 1)
-        steering_angle = float(model.predict(image_array, batch_size=1))
+        S_image = RGB_to_S(resized_image)
+        S_image_array = S_image.reshape(1, 14, 64, 1)
+        steering_angle = float(model.predict(S_image_array, batch_size=1))
         #steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
@@ -89,7 +87,10 @@ def telemetry(sid, data):
         if args.image_folder != '':
             timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
             image_filename = os.path.join(args.image_folder, timestamp)
-            image.save('{}.jpg'.format(image_filename))
+            cropped_img = CropImage(image_array)
+            im = Image.fromarray(cropped_img)
+            im.save('{}.jpg'.format(image_filename))
+	    
     else:
         # NOTE: DON'T EDIT THIS.
         sio.emit('manual', data={}, skip_sid=True)
